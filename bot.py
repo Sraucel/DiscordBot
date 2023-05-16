5ea7a279c4d936601be202010ad269aa9491d162
Sraucel <ryan.guo580@gmail.com>
2023-05-16 17:27:17 -0400

    discord bot

---
+++ bot.py
@@ -0,0 +1,20 @@
+
+import responses
+
+async def send_message(message,user_message, is_private):
+    try:
+        responses = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    TOKEN = 'MTEwMzUxNzQ1ODQxMDc4MjgwMA.GDVbbe.yOjfuWED6sM88UrDaeZ-Pb-4lNRhzmSndssmm8'
+    client = discord.Client()
+
+    async def on_ready():
+        print(f'{client_user}is no running!')
+
+    client.run(TOKEN)
+
+
