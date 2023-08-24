6440b1d68a836268ab67df3640ca38cfe99e09cf
Ryan Guo <ryan.guo580@gmail.com>
2023-08-24 01:53:48 -0400

    Update responses.py

--- responses.py
+++ responses.py
@@ -1,14 +1,17 @@
 import random
-def handle_response(message) -> str:
+def get_response(message : str) -> str:
     p_message = message.lower()
 
     if p_message == 'hello':
-        return 'hey there!'
+        return 'Hey there!'
     
     if p_message == 'roll':
         return str(random.randint(1,6))
     
-    if p_message == '!help':
-        return "`This is a help message that you can modify.`"
+    if p_message == 'whats up today':
+        return "`The sky.`"
     
+    return 'I didn\'t understand that'
+    
+
 
