f0f8f6448ad0c512072463ccc33d4bd49fd990e8
Ryan Guo <ryan.guo580@gmail.com>
2023-08-24 01:51:34 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,44 @@
-
+import discord
 import responses
 
 async def send_message(message,user_message, is_private):
     try:
-        responses = responses.handle_response(user_message)
+        response = responses.get_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
 
 def run_discord_bot():
-    TOKEN = ''
-    client = discord.Client()
+    TOKEN = 'MTEwMzUxNzQ1ODQxMDc4MjgwMA.GffbyU.6Jn4LucH1lzINCAQgihH08ha3XfppxEvgmOSiE'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
 
+    @client.event
     async def on_ready():
-        print(f'{client_user}is no running!')
+        print(f'{client.user} is now running!')
 
+    @client.event
+    async def on_message(message):
+        if message.author.bot:
+            return
+        user_message = str(message.content)
+        """username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message,user_message,is_private=True)
+        else:
+            await send_message(message,user_message,is_private=False)"""
+        
+        await send_message(message,user_message,is_private=False)
+
+         
+    
     client.run(TOKEN)
 
 
